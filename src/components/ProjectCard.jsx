257ce6a6fd658f654ec16000c1f7838a1aa47fd3
Nikita <nikita.ableev@mail.ru>
2021-11-12 18:56:24 +0300

    Update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,38 +1,41 @@
 import { useHistory } from "react-router"
 import { PRODUCT } from "../data/pages"
+import Fade from 'react-reveal/Fade';
 
 const ProjectCard = ({ project }) => {
 
   const history = useHistory()
 
   return (
-    <div className='project-card' onClick={() => history.push(PRODUCT + project.code)}>
-      <div className='project-card__container'>
-        {project.images.length
-          ?
-          <img className='project-card__img' src={'https://api.d4u.dev.dterra.eu/public' + project.imgMain.img} alt="" />
-          :
-          null
-        }
-        {project.imgCnt
-          ?
-          <div className='project-card__label'>
-            {project.imgCnt} фото
-          </div>
-          :
-          null
-        }
+    <Fade bottom distance='100px'>
+      <div className='project-card' onClick={() => history.push(PRODUCT + project.code)}>
+        <div className='project-card__container'>
+          {project.images.length
+            ?
+            <img className='project-card__img' src={'https://api.d4u.dev.dterra.eu/public' + project.imgMain.img} alt="" />
+            :
+            null
+          }
+          {project.imgCnt
+            ?
+            <div className='project-card__label'>
+              {project.imgCnt} фото
+            </div>
+            :
+            null
+          }
+        </div>
+        <div className='project-card__type'>
+          {project.building_type.title}
+        </div>
+        <div className='project-card__title'>
+          {project.title}
+        </div>
+        <div className='project-card__designer'>
+          {project.designer.name && project.designer.name} {project.designer.surname && project.designer.surname}
+        </div>
       </div>
-      <div className='project-card__type'>
-        {project.building_type.title}
-      </div>
-      <div className='project-card__title'>
-        {project.title}
-      </div>
-      <div className='project-card__designer'>
-        {project.designer.name && project.designer.name} {project.designer.surname && project.designer.surname}
-      </div>
-    </div>
+    </Fade>
   )
 }
 
